e918f55ee48e4e045d9cfaa130ab3b3521ea78ed
anandamarwan <anandapuspita2@gmail.com>
2024-06-22 15:46:02 +0700

    feat: make portfolio apps section

--- src/app.tsx
+++ src/app.tsx
@@ -10,7 +10,7 @@ export function App() {
         <nav className="flex justify-end">
           <ul className="flex flex-row gap-10 text-sm text-white">
             <li>
-              <a href=""></a>About
+              <a href="">About</a>
             </li>
             <li>Contact</li>
             <div className="flex flex-row gap-4">
@@ -82,21 +82,22 @@ export function App() {
               Instagram
             </a>
           </div>
-          <picture className="space-y-4">
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          <br />
+          <div className="space-y-4 flex justify-center">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-4 ">
               <div className="grid gap-4">
                 <div>
                   <img
                     className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-                    alt=""
+                    src="images/toys.jpeg"
+                    alt="Toys"
                   />
                 </div>
                 <div>
                   <img
                     className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
-                    alt=""
+                    src="/images/friends.jpeg"
+                    alt="Friends"
                   />
                 </div>
                 <div>
@@ -111,8 +112,8 @@ export function App() {
                 <div>
                   <img
                     className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
-                    alt=""
+                    src="/images/beach.jpeg"
+                    alt="Beach"
                   />
                 </div>
                 <div>
@@ -125,8 +126,8 @@ export function App() {
                 <div>
                   <img
                     className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
-                    alt=""
+                    src="/images/beach.jpeg"
+                    alt="Beach"
                   />
                 </div>
               </div>
@@ -134,15 +135,15 @@ export function App() {
                 <div>
                   <img
                     className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
-                    alt=""
+                    src="/images/ananda-with-car.jpeg"
+                    alt="Ananda with Car"
                   />
                 </div>
                 <div>
                   <img
                     className="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
-                    alt=""
+                    src="/images/boys.jpeg"
+                    alt="Boys"
                   />
                 </div>
                 <div>
@@ -154,7 +155,7 @@ export function App() {
                 </div>
               </div>
             </div>
-          </picture>
+          </div>
         </section>
 
         <div className="space-y-10 flex flex-col justify-center">
@@ -197,6 +198,90 @@ export function App() {
 
         <div className="flex justify-center p-10">
           <h1 className="text-3xl font-bold">Portfolio of Apps</h1>
+        </div>
+
+        <div className="flex flex-row justify-center gap-10">
+          <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <a href="https://contacts.anandamarwan.com/">
+              <img
+                className="rounded-t-lg"
+                src="/images/contacts.png"
+                alt="Contact"
+              />
+            </a>
+            <div className="p-5">
+              <a href="https://contacts.anandamarwan.com/">
+                <h1 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Contacts
+                </h1>
+              </a>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                This Website made for people can control their contacts.
+              </p>
+              <a
+                href="https://contacts.anandamarwan.com/"
+                className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+                <svg
+                  className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+
+          <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+            <a href="https://v2.daily-activity.anandamarwan.com/">
+              <img
+                className="rounded-t-lg"
+                src="/images/daily-activity.png"
+                alt="Daily Activity"
+              />
+            </a>
+            <div className="p-5">
+              <a href="https://v2.daily-activity.anandamarwan.com/">
+                <h1 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Daily Activity
+                </h1>
+              </a>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                Daily Activity app to help with your study or productivity:
+              </p>
+              <a
+                href="https://v2.daily-activity.anandamarwan.com/"
+                className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Read more
+                <svg
+                  className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M1 5h12m0 0L9 1m4 4L9 9"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
         </div>
       </main>
 
