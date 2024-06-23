237167bba539370b35ae12310b89ee1e6980a11f
anandamarwan <anandapuspita2@gmail.com>
2024-06-23 11:20:07 +0700

    feat: add some links to footer

--- src/app.tsx
+++ src/app.tsx
@@ -292,13 +292,13 @@ export function App() {
         <div className="flex justify-center">
           <h1 className="text-3xl text-white">My Links</h1>
         </div>
-        <nav className="flex justify-center">
-          <ul className="flex flex-row gap-4">
+        <nav className="flex flex-col space-y-4">
+          <ul className="flex flex-row justify-center gap-20 items-center">
             <li>
               <a href="https://github.com/anandamarwan/">
                 <img
                   className="w-10 h-10"
-                  src="/images/github.svg"
+                  src="/images/icon-github.svg"
                   alt="Github"
                 />
               </a>
@@ -307,7 +307,7 @@ export function App() {
               <a href="https://www.linkedin.com/in/anandamarwan/">
                 <img
                   className="w-10 h-10"
-                  src="/images/linkedin.svg"
+                  src="/images/icon-linkedin.svg"
                   alt="LinkedIn"
                 />
               </a>
@@ -316,12 +316,29 @@ export function App() {
               <a href="https://www.instagram.com/anandamarwanayaputra/">
                 <img
                   className="w-10 h-10"
-                  src="/images/instagram1.svg"
+                  src="/images/icon-instagram.svg"
                   alt="Instagram"
                 />
               </a>
             </li>
+            <li>
+              <a href="https://t.me/anandamarwan">
+                <img
+                  className="w-10 h-10"
+                  src="/images/icon-telegram.svg"
+                  alt="Telegram"
+                />
+              </a>
+            </li>
           </ul>
+
+          <div className="flex justify-center gap-4">
+            <p className="text-slate-400">
+              Copyright ©2024 Ananda Marwanaya Putra
+            </p>
+
+            <p className="text-slate-400">Made with ❤️</p>
+          </div>
         </nav>
       </footer>
     </div>
